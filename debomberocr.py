3d351b9a92e4e3a0e10e84145b00e59a9fc89f01
Xavee <53323309+xacvwe@users.noreply.github.com>
2021-06-30 19:43:53 +0800

    bonus letter fill
    The script now checks for available bonus words, making you gain life.

--- debomberocr.py
+++ debomberocr.py
@@ -1,19 +1,20 @@
-# DeBomber OCR made by Xavee#7951
+import re
 import pyautogui
 from PIL import Image
 from pytesseract import *
 import keyboard
 import random
+pyautogui.FAILSAFE = False
 
 # DeBomber OCR Settings
 triggerkey   = "delete"
 stopkey      = "escape"
 clearlistkey = "end"
-sscoords     = 540, 390, 50, 30
+sscoords     = 540, 395, 47, 22
 textfile     = "jklm_allwords.txt"
 typespeed    = 0.01
-ocrpath      = "C:\\path\\Tesseract-OCR\\tesseract.exe"
-ocrconfig    = "-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8.5"
+ocrpath      = "C:\\path\\to\\Tesseract-OCR\\tesseract.exe"
+ocrconfig    = "-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8.6"
 ocrlang      = "eng"
 
 # Debomber OCR Settings Help
@@ -23,13 +24,17 @@ ocrlang      = "eng"
 # sscoords     - Screenshots the bomb's letter prompt and converts it into a word containing those letters. it should look something like this: https://github.com/xacvwe/DeBomberOCR/blob/main/letters.png
 # textfile     - Where the OCR grabbs words containing the letters from sscoords.
 # typespeed    - Changes the typespeed, for faster epic dejavu thing use 0.00000000001 if you want but you might get banned
-# ocrpath      - Path for tesseract.exe, and also to make the OCR work.
+# ocrpath      - Path for tesseract.exe, and also to make DeBomberOCR work.
 # ocrconfig    - Keep this config put if you don't know what it does to the script.
 # ocrlang      - Needs tesseract traineddata for france language.
+
 
 pytesseract.tesseract_cmd = ocrpath
 print("DeBomberOCR is now running.")
 usedwords = []
+bonus = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"]
+
+
 
 while True:
     a = keyboard.read_key()
@@ -42,6 +47,8 @@ while True:
         print("Cleared usedwords list.")
 
     if a == triggerkey:
+        reqltr = random.choice(bonus)
+        print(reqltr)
         scltr = pyautogui.screenshot(region=(sscoords))
         scltr.save(r"./letters.png")
         ltrimg = Image.open("letters.png")
@@ -52,18 +59,39 @@ while True:
         print('Letters: ' + pltrs)
         x = open(textfile, "r")
         words = [w for w in x.read().split() if pltrs in w]
+        while True:
+            try:
+                a = random.choice(words)
+            except:
+                print("Cannot choose from an empty sequence (IndexError)")
 
-        try:
-            a = random.choice(words)
-        except:
-            print("Cannot choose from an empty sequence (IndexError)")
-        
+            if reqltr in a:
+                print(f"Word Accepted: {a}")
+                break
+            if reqltr not in a:
+                print(f"Word Declined: {a}")
+                continue
+
         used = [w for w in usedwords if a in w]
 
         if used:
             print("Word is already used!")
-        
+
         if not used:
             print(f"Typing Word: {a}")
             pyautogui.write(a, typespeed)
             pyautogui.press('enter')
+            result = "".join(dict.fromkeys(a))
+            for letter in result:
+                letters = [w for w in bonus if letter in w]
+                if letters:
+                    bonus.remove(letter)
+                if not letters:
+                    print(f"Letter '{letter}' not in list")
+            
+            if bonus == []:
+                print("Gained +1 Life!")
+                fillbonus = "ABCDEFGHIJKLMNOPQRSTUV"
+                for bonusfill in fillbonus:
+                    bonus.append(bonusfill)
+            print("Bonus Letters Left" + ''.join(str(x) for x in bonus))
